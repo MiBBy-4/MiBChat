0356a0509d84e3fbf693b03d88eba0450108b83e
MiBBy-4 <maksim.762373maks@bk.ru>
2022-04-23 18:49:27 +0300

    feat: generate message model, that related with User and Room models, designed styles for chat, implement show method in RoomsController, create new message form layout

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,5 @@ class Room < ApplicationRecord
   validates_uniqueness_of :name, on: :create, message: 'Name of the room must be unique'
   scope :public_rooms, -> { where(:is_private => false)}
   after_create_commit { broadcast_append_to 'rooms' }
+  has_many :messages
 end
