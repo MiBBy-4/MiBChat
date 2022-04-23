bf78d1b74e55d2d52fdf6d34baaea2b5f85fa56a
MiBBy-4 <maksim.762373maks@bk.ru>
2022-04-23 18:20:33 +0300

    feat: install turbo, add broadcast in Room and User models, also implement create method in RoomsController, create new room form layout

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,5 @@
 class Room < ApplicationRecord
   validates_uniqueness_of :name, on: :create, message: 'Name of the room must be unique'
   scope :public_rooms, -> { where(:is_private => false)}
+  after_create_commit { broadcast_append_to 'rooms' }
 end
